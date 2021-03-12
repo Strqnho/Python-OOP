6d83afcfa181cf2984a94850fd69303db6614c16
Boyan Petrov <73431326+Strqnho@users.noreply.github.com>
2021-03-12 15:46:03 +0200

    Update player.py

--- Classes/guild_system/project/player.py
+++ Classes/guild_system/project/player.py
@@ -1,28 +1,22 @@
 class Player:
-    def __init__(self, name, hp: int, mp: int):
+    def __init__(self, name: str, hp: int, mp: int):
         self.name = name
         self.hp = hp
         self.mp = mp
         self.skills = {}
         self.guild = 'Unaffiliated'
 
-    def add_skill(self, skill_name, mana_cost):
-        if skill_name not in self.skills.keys():
-            self.skills[skill_name] = mana_cost
-        else:
-            return 'Skill already added'
+    def add_skill(self, skill_name: str, mana_cost: int):
+        if skill_name in self.skills.keys():
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
 
     def player_info(self):
-        p_data = f'Name: {self.name}\n' \
-                 f'Guild: {self.guild}\n' \
-                 f'HP: {self.hp}\n' \
-                 f'MP: {self.mp}\n'
-        for skill, mana in self.skills.items():
-            p_data += f'==={skill} - {mana}\n'
-
-        return p_data
-
-# player = Player('ivo', 50, 50)
-# player.add_skill('mana', 10)
-# player.add_skill('new', 100)
-# print(player.player_info())
+        info = f'Name: {self.name}\n' \
+               f'Guild: {self.guild}\n' \
+               f'HP: {self.hp}\n' \
+               f'MP: {self.mp}\n'
+        for sk_name, sk_cost in self.skills.items():
+            info += f'==={sk_name} - {sk_cost}\n'
+        return info
