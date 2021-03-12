4063dfbf8402106393cb36ffbbac9df14ffe0114
Boyan Petrov <73431326+Strqnho@users.noreply.github.com>
2021-03-12 15:46:26 +0200

    Update guild.py

--- Classes/guild_system/project/guild.py
+++ Classes/guild_system/project/guild.py
@@ -1,31 +1,32 @@
+from Guild_System.player import Player
+
 class Guild:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
-        self.list_of_players = []
+        self.players = []
 
-    def assign_player(self, player):
-        if player.guild == 'Unaffiliated':
-            player.guild = self.name
-            self.list_of_players.append(player)
-            return f'Welcome player {player.name} to the guild {self.name}'
+    def assign_player(self, p: Player):
+        if p.guild == 'Unaffiliated':
+            p.guild = self.name
+            self.players.append(p)
+            return f"Welcome player {p.name} to the guild {self.name}"
+        if p.guild == self.name:
+            return f"Player {p.name} is already in the guild."
+        return f"Player {p.name} is in another guild."
 
-        elif player.guild == self.name:
-            return f'Player {player.name} is already in the guild.'
-
-        else:
-            return f'Player {player.name} is in another guild.'
-
-    def kick_player(self, pl_name):
+    def kick_player(self, player_name):
         try:
-            filtered_player = [p for p in self.list_of_players if p.name == pl_name][0]
+            filtered_player = [p for p in self.players if p.name == player_name][0]
             filtered_player.guild = 'Unaffiliated'
-            self.list_of_players.remove(filtered_player)
+            self.players.remove(filtered_player)
+            return f"Player {player_name} has been removed from the guild."
         except IndexError:
-            return f'Player {pl_name} is not in the guild.'
+            return f"Player {player_name} is not in the guild."
 
     def guild_info(self):
-        g_data = f'Guild: {self.name}\n'
-        for p in self.list_of_players:
-            g_data += f'{p.player_info()}\n'
-        return g_data
+        g_info = f"Guild: {self.name}\n"
+        pl_info = ''
+        for pl in self.players:
+            pl_info += f'{pl.player_info()}'
+        return g_info + pl_info
 
